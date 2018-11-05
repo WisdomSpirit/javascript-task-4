7598c60647075c02bd31e56c8a84b4c1a73e37a8
FoOkySNick <FoOkySNick@ya.ru>
2018-11-05 20:35:46 +0500

    Add files via upload

--- emitter.js
+++ emitter.js
@@ -6,11 +6,59 @@
  */
 const isStar = true;
 
+function getAllEventsInChain(event) {
+    const events = event.split('.');
+    const resultEventList = [];
+    let string = '';
+    events.forEach(e => {
+        string += e;
+        resultEventList.unshift(string);
+        string += '.';
+    });
+
+    return resultEventList;
+}
+
+function safeGet(event, eventList) {
+    if (typeof eventList[event] === 'undefined') {
+        eventList[event] = [];
+    }
+
+    return eventList[event];
+}
+
+function getObjectWithAllProperties(context, handler,
+    times = Number.MAX_SAFE_INTEGER, frequency = 1) {
+    return {
+        context: context,
+        handler: handler,
+        times: times,
+        frequency: frequency,
+        callsCount: 0
+    };
+}
+
+function realEmit(event, eventList) {
+    Object.keys(eventList)
+        .filter(e => e === event)
+        .forEach(e => {
+            eventList[e].forEach(student => {
+                if ((student.times - student.callsCount > 0) &&
+                    (student.callsCount % student.frequency === 0)) {
+                    student.handler.call(student.context);
+                }
+                student.callsCount += 1;
+            });
+        });
+}
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    const eventsList = {};
+
     return {
 
         /**
@@ -18,26 +66,48 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            safeGet(event, eventsList).push(
+                getObjectWithAllProperties(context, handler)
+            );
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            Object.keys(eventsList)
+                .filter(key => key.startsWith(event))
+                .forEach(key => {
+                    for (let i = 0; i < eventsList[key].length; i++) {
+                        if (eventsList[key][i].context === context) {
+                            eventsList[key].splice(i, 1);
+                        }
+                    }
+                });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            const events = getAllEventsInChain(event);
+            events.forEach(e => {
+                realEmit(e, eventsList);
+            });
+
+            return this;
         },
 
         /**
@@ -47,9 +117,15 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} this
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            if (typeof times !== 'number' || times < 1) {
+                this.on(event, context, handler);
+            }
+            safeGet(event, eventsList).push(getObjectWithAllProperties(context, handler, times));
+
+            return this;
         },
 
         /**
@@ -59,9 +135,17 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} this
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            if (typeof frequency !== 'number' || frequency < 1) {
+                this.on(event, context, handler);
+            }
+            safeGet(event, eventsList).push(
+                getObjectWithAllProperties(context, handler, undefined, frequency)
+            );
+
+            return this;
         }
     };
 }
